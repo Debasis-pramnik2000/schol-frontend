5d671306836383bf3f26289d1f19a3946bf2bcd3
Debasis-pramanik2000 <pramanikdebasis2003@gmail.com>
2026-09-21 10:19:20 +0530

    hyu

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_URL =
-  process.env.REACT_APP_API_URL ||
-  'https://scholl-backend-1.onrender.com';
+const API_URL = 'https://scholl-backend-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
